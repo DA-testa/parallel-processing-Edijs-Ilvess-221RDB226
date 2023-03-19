b7ba62656c7cb645aed5a692dfcc3d8b4ff0df00
Edijs-Ilvess-221RDB226 <125735046+Edijs-Ilvess-221RDB226@users.noreply.github.com>
2023-03-19 19:14:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n=list(map(int,input().split()))
-    m=list(map(int,input().split()))
+    n,m=map(int,input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
